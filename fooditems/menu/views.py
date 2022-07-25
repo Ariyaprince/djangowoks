a7734747b61e2f3804ef5e916a2d9e951a419a1e
ariyaprince <ariyaprince@gmail.com>
2022-07-25 12:27:51 +0700

    add

--- fooditems/menu/views.py
+++ fooditems/menu/views.py
@@ -7,11 +7,15 @@ from menu.models import menu_items
 
 class ItemView(APIView):
     def get(self,request,*args,**kwargs):
-        print(request.query_params)
+        all_items=menu_items
         if "category" in request.query_params:
-            items=request.query_params.get("category")
-            data=[item for item in menu_items if item["category"]==items]
-            return Response(data=data)
+            category=request.query_params.get("category")
+            all_items=[item for item in menu_items if item["category"]==category]
+        if "limit" in request.query_params:
+            lim=int(request.query_params.get("limit"))
+            all_items=all_items[:lim]
+            return Response(data=all_items)
+
 
         if "rating" in request.query_params:
             rating=int(request.query_params.get("rating"))
